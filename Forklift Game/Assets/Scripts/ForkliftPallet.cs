81ff64f62ef463ac4dbbe765fa14a1f7be4abf7d
Muzaffer Akdoğan <muzafferakdgnn@gmail.com>
2021-10-11 18:20:40 +0300

    Forklift pallet updated

--- Forklift Game/Assets/Scripts/ForkliftPallet.cs
+++ Forklift Game/Assets/Scripts/ForkliftPallet.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ForkliftPallet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool up;
+    private bool down;
+
+    [SerializeField] private float liftPower;
+
+    [SerializeField] private GameObject pallet;
+
+    [SerializeField] private Transform palletTransform;
+    
+    [SerializeField] private Vector3 maxY;
+    [SerializeField] private Vector3 minY;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        getUserPalletInput();
+        mooveThePallet();
+    }
+
+    void getUserPalletInput()
+    {
+        up = Input.GetKey(KeyCode.Y);
+        down = Input.GetKey(KeyCode.H);
+    }
+
+    void mooveThePallet()
+    {
+        if (up)
+        {
+            palletTransform.transform.localPosition = Vector3.MoveTowards(palletTransform.localPosition, maxY, liftPower);
+        }
+
+        else if (down)
+        {
+            palletTransform.transform.localPosition = Vector3.MoveTowards(palletTransform.localPosition, minY, liftPower);
+        }       
     }
 }
